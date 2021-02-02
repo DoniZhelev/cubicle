9be936924c53bd6c4020751424593aff5a12a97a
DoniZhelev <donizhelev@gmail.com>
2021-02-02 11:31:11 +0200

    extract express config

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const handlebars  = require('express-handlebars')
+
 const config = require('./config/config');
 
 const app = express();
 
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-app.use(express.static('static'))
-
+require('./config/express')(app)
 
 app.get('/', (req, res) => {
     res.render('home', {layout: false})

8d4172c458527d6bea0ae6b5686b907dcc7fecfb
DoniZhelev <donizhelev@gmail.com>
2021-02-07 21:20:13 +0200

    added active search button

--- servises/productService.js
+++ servises/productService.js
@@ -1,18 +1,32 @@
 const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
 const fs = require('fs');
+const path = require('path')
 
 let productsData = require('../config/products.json');
 
-function getAll() {
-    return productsData;
+function getAll(query) {
+    let result = productsData;
+
+    if(query.search) {
+        result = result.filter(x => x.name.toLocaleLowerCase().includes(query.search))
+    }
+    if(query.from) {
+        result = result.filter(x => Number(x.level) >= query.from);
+    }
+    
+    if(query.to) {
+        result = result.filter(x => Number(x.level) <= query.to);
+    }
+    
+    return result;
 }
 
 function getOne(id) {
     return productsData.find(x => x.id == id)
 }
 
-function create(data) {
+function create(data, callback) {
     let cube = new Cube(
         uniqid(),
         data.name,
@@ -21,17 +35,19 @@ function create(data) {
         data.difficultyLevel
     );
     productsData.push(cube);
+ // call back WAY
 
-    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err)=>{
-        if(err) {
-            console.log(err);
-            return;
-        }
-    });
+    fs.writeFile(path.join(__dirname, '../config/products.json'), JSON.stringify(productsData), callback)
 }
 
-    module.exports = {
-        getAll,
-        getOne,
-        create
-    }
+// return fs.writeFile(
+//     path.join(__dirname, '../config/products.json'),
+// JSON.stringify(productsData),
+// )
+// }
+
+module.exports = {
+    getAll,
+    getOne,
+    create
+}

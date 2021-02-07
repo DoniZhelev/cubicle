ed6d477f671e5392dd98a9d7d96883051ff56cb9
DoniZhelev <donizhelev@gmail.com>
2021-02-07 12:37:04 +0200

    add body parser function

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const Cube = require('../models/Cube')
 
 const router = Router();
 
@@ -9,6 +10,13 @@ router.get('/', (req, res) =>{
 router.get('/create', (req, res) => {
     res.render('create', { title: 'Create'})
 });
+
+router.post('/create', (req, res) =>{
+    console.log(req.body);
+    let cube = new Cube()
+
+    res.redirect('/products');
+})
 
 router.get('/details/:productsId', (req, res) =>{
     res.render('details', {title: 'Product Details'})

ea44291d26519c3bba8138a0f2e615ee0308465d
DoniZhelev <donizhelev@gmail.com>
2021-02-14 18:31:13 +0200

    create accessories  button and new route for it

---
+++ controllers/accessoryController.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const accessoryService = require('../servises/accessoryService');
+const router = Router();
+
+router.get('/create', (req,res) =>{
+    res.render('createAccessory');
+});
+
+router.post('/create', (req, res) =>{
+    accessoryService.create(req.body)
+    .then(() => res.redirect('/products'))
+    .catch(() => res.status(500).end());
+
+    
+})
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const homeController = require('./controllers/homeController');
 const productController = require('./controllers/productController');
+const accessoryController = require('./controllers/accessoryController');
+
 
 const router = Router();
 
 router.use('/', homeController);
 router.use('/products', productController);
+router.use('/accessories', accessoryController)
 router.get('*', (req,res)=>{
     res.render('404');
 })
